d238d44a7a026eb10cb51a37190283f4f26984fb
koscida <britkos@gmail.com>
2025-05-31 09:15:13 -0400

    add nav

--- raw/js/localStorageHelper.js
+++ raw/js/localStorageHelper.js
@@ -6,38 +6,46 @@ const _lsh = {
 	prefix: "cosplan",
 
 	_load(key) {
-		console.log("-_lsh._load(key): ", key);
+		// console.log("-_lsh._load(key): ", key);
 		const stored = localStorage.getItem(`${this.prefix}-${key}`);
 		return stored ? JSON.parse(stored) : null;
 	},
 	_store(key, value) {
-		console.log("-_lsh._store(key, value): ", key, " || ", value);
+		// console.log("-_lsh._store(key, value): ", key, " || ", value);
 		localStorage.setItem(`${this.prefix}-${key}`, JSON.stringify(value));
 	},
 	_modify(key, fn) {
-		console.log("-_lsh._modify(key, fn): ", key, " || ");
+		// console.log("-_lsh._modify(key, fn): ", key, " || ");
 		this._store(key, fn(this._load(key)));
 	},
 };
 const lsh = {
-	get(key) {
-		console.log("-_lsh.get(key): ", key);
+	get: (key) => {
+		// console.log("-_lsh.get(key): ", key);
 		return key ? _lsh._load(key) : null;
 	},
+	getByID: (key, id) => {
+		if (key) {
+			const stored = _lsh._load(key);
+			if (stored) return stored[id];
+		}
+		return null;
+	},
 	addToArray: (item, key) => {
-		console.log("-_lsh.appendItemToArray(item, key): ", item, " || ", key);
+		// console.log("-_lsh.appendItemToArray(item, key): ", item, " || ", key);
 		_lsh._modify(key, (storage = []) => [...storage, item]);
 	},
 	deleteFromArray: (item, key) => {
-		console.log("-_lsh.removeItemFromArray(item, key): ", item, " || ", key);
+		// console.log("-_lsh.removeItemFromArray(item, key): ", item, " || ", key);
 		_lsh._modify(key, (storage = []) => storage.filter((s) => s !== item));
 	},
 
 	addToObject: (item, key) => {
-		console.log("-_lsh.addItemToObject(item, key): ", item, " || ", key);
+		// console.log("-_lsh.addItemToObject(item, key): ", item, " || ", key);
 		let newID = 0;
 		_lsh._modify(key, function (storage = {}) {
-			if (storage) newID = Math.max(Object.keys(storage));
+			// console.log("-_lsh._modify(fn): ", storage, " || ", key);
+			if (storage) newID = parseInt(Math.max(Object.keys(storage))) + 1;
 			return { ...storage, [newID]: item };
 		});
 		return newID;
--- raw/js/main.js
+++ raw/js/main.js
@@ -1,1 +1,27 @@
-
+$(document).ready(function () {
+	$("header").html(`
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="container-fluid">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="./index.html">Cosplays</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="./images.html">Images</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="./closet.html">Closet</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="./schedule.html">Schedule</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		`);
+});
